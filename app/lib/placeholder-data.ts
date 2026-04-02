55cbde88b0ca6a57ee432ab85e3b4665b9ed663b
GitIS89 <isabelle.midy.gm@gmail.com>
2026-04-02 12:35:29 +0200

    affichage 5 dernieres factures  seulement

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -19,6 +19,12 @@ const customers = [
     email: '',
     image_url: '',
   },
+
+    // {id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
+    // name: 'Evil Rabbit',
+    // email: 'evil@rabbit.com',
+    // image_url: '/customers/evil-rabbit.png',},
+
   // {
   //   id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
   //   name: 'Delba de Oliveira',
